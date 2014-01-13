25877daca2a754453fcf931956880d34da107029
Laurent Peuch <cortex@worlddomination.be>
2014-01-13 01:42:06 +0100

    [fix] bot isn't storing chans anymore

--- ii-bot/reconnection.sh
+++ ii-bot/reconnection.sh
@@ -2,7 +2,7 @@
 while true
 do
     # if you only have one chan
-    {% for chan in bot.get("chans", []) %}
+    {% for chan in server.get("chans", []) %}
     (sleep 5; echo "/j #{{ chan }}" > irc/{{ server_address }}/in) &
     {% endfor %}
     ii \
